e1779907d42d4bee5e3888d6f551546ccbddbe60
RamonFont <ecommerce01@ctic.tec.br>
2025-11-17 23:23:44 -0300

    add build correção

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: "Cookin-Up--Lista-de-receitas/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
